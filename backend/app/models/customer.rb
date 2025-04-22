d6e5d7960a5cad645adfdf4d2332ea6a4b5b5d92
AlvesLuis0 <alves.luis.3002@gmail.com>
2025-04-22 18:47:24 -0300

    fix: adicionar validação no endereço e contatos do cliente

--- backend/app/models/customer.rb
+++ backend/app/models/customer.rb
@@ -5,6 +5,8 @@ class Customer < ApplicationRecord
   accepts_nested_attributes_for :person
 
   validates :note, if: :note?, presence: true, length: { maximum: 1000 }
+  validate :address_must_be_valid
+  validate :contacts_must_be_valid
 
   after_initialize -> {
     address = Address.new
@@ -18,4 +20,20 @@ class Customer < ApplicationRecord
     self.person.address ||= address
     self.person.contacts = contacts if self.person.contacts.empty?
   }
+
+  private
+
+  NUMBER_OF_EMAILS = 1
+  NUMBER_OF_MOBILES = 3
+  NUMBER_OF_CONTACTS = NUMBER_OF_EMAILS + NUMBER_OF_MOBILES
+
+  def contacts_must_be_valid
+    errors.add(:customer, "precisa ter #{NUMBER_OF_CONTACTS} contatos") if person.contacts.size != NUMBER_OF_CONTACTS
+    errors.add(:customer, "precisa ter #{NUMBER_OF_EMAILS} emails") if person.contacts.filter(&:contact_type_email?).size != NUMBER_OF_EMAILS
+    errors.add(:customer, "precisa ter #{NUMBER_OF_MOBILES} números de celular") if person.contacts.filter(&:contact_type_mobile?).size != NUMBER_OF_MOBILES
+  end
+
+  def address_must_be_valid
+    errors.add(:customer, "precisa ter um endereço") if person.address.nil?
+  end
 end
